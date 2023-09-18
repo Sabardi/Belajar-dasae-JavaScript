6d8fd4f094bfd19961721718b191c26f1ac4c25f
Sabardi <Sabardibahari@gmail.com>
2023-09-18 22:26:15 +0800

    oprator di js

--- index.js
+++ index.js
@@ -9,8 +9,27 @@ let lastname = "bahari"
 let fullname = name + lastname
 console.log("perkenalkan saya : ",fullname)
 
-//oprator
-let angka1 = 1
-let angka2 = 10
+//oprator start
+/*
+penjumlahan
+pengurangan
+perkalian 
+pembagian
+sisa hasil bagi (%)
+perpangkatan (**)
+*/
+let angka1 = 90
+let angka2 = 20
 let hitung = angka1 + angka2
-console.log("hasil dari perhitungan angka1 dan angka2", hitung)
+let hitung1 = angka1 - angka2
+let hitung2 = angka1 * angka2
+let hitung3 = angka1 / angka2
+let hitung4 = angka1 % angka2
+let hitung5 = angka1 ** angka2
+console.log("hasil dari penjumlahan angka1 dan angka2 =", hitung)
+console.log("hasil dari pengurangan angka1 dan angka2 =", hitung1)
+console.log("hasil dari perkalian angka1 dan angka2 =", hitung2)
+console.log("hasil dari pembagian angka1 dan angka2 =", hitung3)
+console.log("hasil dari sisa bagi angka1 dan angka2 =", hitung4)
+console.log("hasil dari perpangkatan angka1 dan angka2 =", hitung5)
+//oprator end
