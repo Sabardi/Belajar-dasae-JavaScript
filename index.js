70b0c8e1760972e7e00947074cc6fa44800e2780
Sabardi <Sabardibahari@gmail.com>
2023-09-18 22:14:05 +0800

    Hallo, Senin 18 september 2023 saya belajar java scrift

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ console.log("perkenalkan Nama saya",Nama)
 
 let name = "sabardi"
 let lastname = "bahari"
-
 let fullname = name + lastname
-
 console.log("perkenalkan saya : ",fullname)
+
+//oprator
+let angka1 = 1
+let angka2 = 10
+let hitung = angka1 + angka2
+
+console.log("hasil dari perhitungan angka1 dan angka2", hitung)
