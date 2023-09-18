4e36cc5528ddf5e4c289faddecbc411f8187c0f7
Sabardi <Sabardibahari@gmail.com>
2023-09-18 22:10:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Print helo
+let Nama = "sabardi" ;
+console.log("perkenalkan Nama saya",Nama)
+
+//variabel dan tipe data
+
+let name = "sabardi"
+let lastname = "bahari"
+
+let fullname = name + lastname
+
+console.log("perkenalkan saya : ",fullname)
