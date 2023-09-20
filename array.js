2aa3ecacb37ba654c1ad2ca7a32c72e62e538dcf
Sabardi <Sabardibahari@gmail.com>
2023-09-21 02:24:30 +0800

    materi array finish

--- array.js
+++ array.js
@@ -1,5 +1,22 @@
+/** fungsi dalam array  
+ * push() => akan menambahkan data di akhir array
+ * pop() untuk mengeluarkan data terakhir di array
+ * unshift() menambahkan element di awal array
+ * delete untuk menghapus data di dalam array nya
+ * splice() untuk menghapus data di array nya langsung dimana indek nya dimulai
+ * splice() bisa melakukan penyisipan data di antara idex array
+ * */
 let data = [
     "Cokelat", 20, 40, "apel"
 ]
+data.push(10)
+data.pop()
+data.push("masukin ini di akhir")
+data.unshift("masukan ini di awal array")
+delete data[1]
+// data.splice([5])
+data.splice(1,3,"hallo")
+console.log(data)
 console.log(data[4])
 console.log("panjang data nya adalah : "+ data.length) //leng berfungsi untuk mengetahui banyak data nya
+console.log(data)
