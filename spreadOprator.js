46b22c29f336922ae662972db8058ed3e10088d0
Sabardi <Sabardibahari@gmail.com>
2023-09-22 16:13:26 +0800

    ....array1...array2 dstnya

--- spreadOprator.js
+++ spreadOprator.js
@@ -1,2 +1,7 @@
 const favorit = ["Seafood", "Salad", "Nugget", "Soup",10];
+const other = ["apel", "manggo", "orange", "tomat",10];
+const allfavorita = [favorit, other]
+const allfavorit = [...favorit,...other]//menggabunfkan 2 buah array
 console.log(...favorit) //menggabungkan seluruh nya
+console.log(allfavorit) 
+console.log(allfavorit) //memanggil ke2 buah array 
